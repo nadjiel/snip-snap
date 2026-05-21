2fb73fab805e66da057bb6fdd7f425432527f888
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-05-20 22:42:08 -0300

    :sparkles: feat: mkv to gif conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import { useState, useEffect } from "react";
-import { FFmpeg/* , fetchFile */ } from "@ffmpeg/ffmpeg";
-import { toBlobURL } from "@ffmpeg/util";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { toBlobURL, fetchFile } from "@ffmpeg/util";
 
 const baseUrl = "https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.10/dist/esm";
 
 const ffmpeg = new FFmpeg();
 
 export default function App() {
-  const [ready, setReady] = useState<boolean>(null);
+  const [ready, setReady] = useState(false);
   const [log, setLog] = useState("");
   const [video, setVideo] = useState<File>(null);
-
-  useEffect(() => {
-    ffmpeg.on("log", ({ message }) => {
-      setLog(message);
-      console.log(message);
-    });
-  }, []);
+  const [gifURL, setGifURL] = useState("");
 
   const load = async () => {
     await ffmpeg.load({
@@ -27,27 +21,51 @@ export default function App() {
     setReady(true);
   }
 
+  useEffect(() => {
+    ffmpeg.on("log", ({ message }) => {
+      setLog(message);
+      console.log(message);
+    });
+
+    load();
+  }, []);
+
+  const convert = async () => {
+    ffmpeg.writeFile("input.mkv", await fetchFile(video));
+
+    await ffmpeg.exec(["-i", "input.mkv", "-t", "2.5", "-ss", "2.0", "-f", "gif", "output.gif"]);
+
+    const data = await ffmpeg.readFile("output.gif");
+
+    setGifURL(URL.createObjectURL(new Blob([data.buffer], { type: "image/gif"})));
+  }
+
   return (
     <div>
       {
-        ready === null
-          ? <p>Click to load!</p>
-          : ready
+        ready
           ? <p>FFMpeg loaded!</p>
           : <p>Loading...</p>
       }
       <p>{log}</p>
-      <button onClick={load}>Load FFMpeg</button>
+      <button onClick={convert}>Convert video!</button>
       <input type="file" onChange={e => setVideo(e.target.files.item(0))} />
       {
         video && (
           <video
             controls
-            width="720"
+            width={720}
             src={URL.createObjectURL(video)}
           ></video>
         )
       }
+      <br />
+      { gifURL && (
+        <img
+          src={gifURL}
+          width={720}
+        />
+      ) }
     </div>
   )
 }
