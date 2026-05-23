8c8014644b2baf1654216e23a102d39ff22dc744
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-05-23 19:50:49 -0300

    :sparkles: feat: added automatic logging for ffmpeg Besides logging capabilities, general documentation was added, as well as util functions to control the new log behavior and to split args nicely.

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,13 +1,64 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { toBlobURL } from "@ffmpeg/util";
 
+/**
+ * The base URL recommended for loading **FFmpeg.wasm**,
+ * as per its {@link https://ffmpegwasm.netlify.app/docs/getting-started/usage docs}.
+ * */
 const baseUrl = "https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.10/dist/esm";
+
+interface LogConfig {
+
+  /** Tells whether logs should be stored. */
+  store: boolean;
+  
+  /** Tells whether logs should be printed. */
+  print: boolean;
+
+}
+
+/** Determines if realtime logs are store and/ or printed. */
+const logConfig: LogConfig = {
+  store: false,
+  print: false,
+};
+
+/** All logs from `ffmpeg` while logging is configured to store them. */
+export let logs = "";
 
 export const ffmpeg = new FFmpeg();
 
+ffmpeg.on("log", ({ message }) => {
+  if(logConfig.store) logs += "\n" + message;
+  if(logConfig.print) console.log(message);
+});
+
+/** Loads ffmpeg with recommended URLs. */
 export async function load() {
   await ffmpeg.load({
     coreURL: await toBlobURL(`${baseUrl}/ffmpeg-core.js`, "text/javascript"),
     wasmURL: await toBlobURL(`${baseUrl}/ffmpeg-core.wasm`, "application/wasm"),
   });
+}
+
+/**
+ * Transforms a `string` with arguments into an `ffmpeg` friendly arg array.
+ */
+export function argify(args: string) {
+  return args.split(" ");
+}
+
+/**
+ * Configures the automatic logging behavior.
+ * 
+ * @param config A {@link LogConfig `LogConfig`} object describing the desired behavior.
+ */
+export function configLogging(config: boolean | LogConfig) {
+  if (typeof config === "boolean") {
+    logConfig.store = config;
+    logConfig.print = config;
+  } else {
+    logConfig.store = config.store;
+    logConfig.print = config.print;
+  }
 }
