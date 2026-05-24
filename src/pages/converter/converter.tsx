7fe676e9cfad39c033c143a36a22fbb8746e45ed
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-05-24 17:28:00 -0300

    :art: style: revamped converter page

--- src/pages/converter/converter.tsx
+++ src/pages/converter/converter.tsx
@@ -21,45 +21,54 @@ export default function ConverterPage() {
 	}, []);
 
 	return (
-		<div>
-			<h1>Video Converter</h1>
-			{ready ? <p>FFMpeg loaded!</p> : <p>Loading...</p>}
-			<div>
+		<div className="max-w-4xl mx-auto p-4">
+			<h1 className="font-bold text-4xl text-center mb-4">Video Converter</h1>
+			<p className="mb-4">Upload a video and select an output format.</p>
+			<p className="text-center mb-2">
+				{ready ? "Ready to convert!" : "Loading tool..."}
+			</p>
+			<div className="max-w-lg mx-auto">
 				<Input
 					type="file"
 					onChange={(e) => setVideo(e.target.files?.item(0) ?? null)}
+					className="min-h-16 mb-2"
 				/>
 				<video
 					controls
-					width={720}
+					width={512}
+					height={288}
 					src={video ? URL.createObjectURL(video) : undefined}
+					className="mb-2"
 				>
 					<track kind="captions"></track>
 				</video>
 			</div>
 			<p>{log}</p>
-			<div>
+			<div className="flex justify-center">
 				<Button
-					disabled={!!video}
-					onClick={() => video && convertFormat(video).then(setGifURL)}
+					disabled={!video}
+					onClick={() => video && convertFormat(video, "gif").then(setGifURL)}
 				>
 					Convert video!
 				</Button>
 				<Button
-					disabled={!!video}
+					disabled={!video}
 					onClick={() => video && detectIdleness(video).then(console.log)}
 				>
 					Detect idleness!
 				</Button>
 			</div>
-			<div>
-				<h2>Result</h2>
-				{gifURL && (
+			<div className="max-w-lg mx-auto">
+				<h2 className="text-2xl font-bold mb-2">Result</h2>
+				{gifURL ? (
 					<img
 						src={gifURL}
 						alt="GIF generated from the provided video"
-						width={720}
+						width={512}
+						height={288}
 					/>
+				) : (
+					<p>Process the video to see the result here.</p>
 				)}
 			</div>
 		</div>
