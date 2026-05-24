4e183be817302c0ef8cacd0d755ae004f408115d
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-05-24 17:24:06 -0300

    :recycle: refac: changed shadcn util location Moved it from the default `lib/util` to `util/css`.

--- src/comps/ui/input.tsx
+++ src/comps/ui/input.tsx
@@ -1,6 +1,6 @@
 import type * as React from "react";
 
-import { cn } from "@/lib/util";
+import { cn } from "@/util/css";
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
 	return (
