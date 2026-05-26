0d370bb4d9e8a0574c55647d645a32d03688ffd6
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-05-25 22:24:45 -0300

    :bug: fix: base path configuration GitHub Pages deploy was choking because it needed a base path properly set in the build, so this commit adds that, together with env ignores in `.gitignore` and a `.env.example` file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		base: env.BASE_PATH || "/",
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
+	};
 });
